f61e01c73cc90254666b992f8225b37c498ba194
ikubacki <igor.kubacki@thetrainline.com>
2023-04-23 22:11:19 +0200

    Utworzenie srodowiska wirtualnego

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\vdi-terminal\Documents\igor_kubacki\se_nauka_gita\.venv
+set VIRTUAL_ENV=C:\Users\ikubacki\code\se_nauka_gita\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
